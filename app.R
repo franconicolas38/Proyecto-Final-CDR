f0928b9185b6c45d71d8cd493667f44181dfcdcd
FRANCO NICOLAS PURTSCHER FERREIRA <56343078@ceibal.edu.uy>
2026-07-20 14:46:04 -0300

    shiny final

--- app.R
+++ app.R
@@ -19,34 +19,105 @@ df =
                                               "Obesity_Type_II",
                                               "Obesity_Type_III"),
                                    ordered = TRUE),
+               across(where(is.character), as.factor),
                across(c(FCVC,NCP,CH2O,FAF,TUE),round),
                across(c(FCVC,NCP,CH2O,FAF,TUE),as.factor)) 
                  
 variables_factor = names(df)[-(2:4)]
 variables_numericas = names(df)[2:4]
 
+# Random Forest
+set.seed(1)
+split = initial_split(df, prop = 0.8, strata = NObeyesdad)
+train_df = training(split)
+test_df = testing(split)
+receta_rf2 <- recipe(NObeyesdad ~ ., data = train_df) |> 
+  step_rm(Weight, Height, Age, Gender)
+
+rforest2 = 
+  rand_forest(trees = 1000) |> 
+  set_engine("ranger", importance = "impurity") |> 
+  set_mode("classification") 
+
+wf_rforest2 = 
+  workflow() |>
+  add_recipe(receta_rf2) |> 
+  add_model(rforest2) |> 
+  fit(data = train_df)
 
 ui = navbarPage(
   title = "Explorador de datos - Obesidad",
-  
+  # Formato de la barra superior y de los paneles
+  header = tags$head(
+    tags$style(HTML("
+    .navbar-default { background-color: #AEC6CF !important; }
+    .navbar-default .navbar-brand, .navbar-default .navbar-nav > li > a { color: #2c3e50 !important; }
+    
+    .well { 
+      background-color: #F0F8FF !important; 
+      border: 1px solid #AEC6CF !important; 
+    }
+  "))
+  ),
   # --- Primer pestaña --- #
   tabPanel("Inicio",
-           h2("Bienvenido/a"),
-           p("Esta aplicación permite explorar el dataset de obesidad, cruzando
-              variables numéricas y categóricas, y calcular tu propio IMC."),
-           p("Usá la pestaña 'Exploración' para explorar el dataset,
-              o 'Calculadora IMC' para calcular tu índice de masa corporal.")
+           h2("Proyecto final de Ciencia de Datos con R 2026", 
+              style = "font-weight: bold; text-decoration: underline;"),
+           h4("Franco Purtscher, Emiliano Santestevan, Ignacio Tarigo")
+           wellPanel(
+             h3("Descripción del proyecto"),
+             p("Esta Shiny está basada en una base de datos extraída de UCI Machine Learning,
+               con la cual intentaremos explicar el nivel de obesidad de un individuo,
+               basándonos en hábitos alimenticios, condición física, historial familiar, y demás variables.",
+               style = "font-size: 16px;"),
+             p("Creemos que es un fenómeno interesante de abordar, debido a que determinados niveles de obesidad
+               pueden llegar a ser perjudiciales para la salud de una persona. A través de este análisis intentaremos
+               conocer ciertos motivos o patrones que podrían permitir la detección o la prevención de ese riesgo.",
+               style = "font-size: 16px;"),
+             
+             hr(),
+             
+             h4("Guía de la aplicación:"),
+             tags$ul(style = "font-size: 18px;",
+                     tags$li("En la pestaña 'Base de datos' podrás acceder al dataset y a información sobre él."),
+                     tags$li("En la pestaña 'Exploración', verás cómo se relacionan cada una de las variables del dataset."),
+                     tags$li("En la pestaña 'Calculadora IMC', podrás ingresar tu altura y peso para obtener tu IMC."),
+                     tags$li("En la pestaña 'Modelo predictivo', podrás responder preguntas para que el modelo prediga tu categoría.")
+             )
+           )
   ),
    # --- Segunda pestaña --- #
   tabPanel("Base de datos",
            sidebarLayout(
              sidebarPanel(
-               checkboxGroupInput("NObeyesdad_base", "Categoría",
-                                  choices = levels(df$NObeyesdad),
-                                  selected = levels(df$NObeyesdad))
+               p("La base de datos está basada en encuestas web a individuos de México, Perú y Colombia.
+               El 23% de las observaciones se recopilaron directamente desde las encuestas,
+               mientras que el 77% fueron creados sinteticamente utilizando la herramienta Woka y el filtro SMOTE,
+                 con el fin de equiparar la cantidad de datos en cada categoría y así
+                 los distintos algoritmos que se pueden llegar a utilizar con el dataframe funcionen de una mejor manera.
+                 Contiene 2111 observaciones y 17 variables",
+                 style = "font-size: 16px;"),
+               wellPanel(
+                 h4("Glosario de variables"),
+                 tags$div(style = "font-size: 15px;",
+                          p(strong("FAVC:"), " Consumo frecuente de alimentos calóricos."),
+                          p(strong("FCVC:"), " Frecuencia de consumo de vegetales."),
+                          p(strong("NCP:"), " Número de comidas principales."),
+                          p(strong("CAEC:"), " Consumo de alimentos entre comidas."),
+                          p(strong("SMOKE:"), " Si fuma."),
+                          p(strong("CH2O:"), " Consumo de agua diario."),
+                          p(strong("SCC:"), " Monitoreo de calorías consumidas."),
+                          p(strong("FAF:"), " Frecuencia de actividad física."),
+                          p(strong("TUE:"), " Tiempo de uso de dispositivos tecnológicos."),
+                          p(strong("CALC:"), " Consumo de alcohol."),
+                          p(strong("MTRANS:"), " Medio de transporte utilizado.")
+                 )
+               ),
              ),
              mainPanel(
-               DTOutput("tabla")
+               DTOutput("tabla"),
+               h3("La categoría de NObeyesdad, esta basada en el valor del IMC"),
+               h3("Puede encontrar toda la información de la base de datos en: https://www.sciencedirect.com/science/article/pii/S2352340919306985?via%3Dihub")
              )
            )
   ),
@@ -143,6 +214,42 @@ ui = navbarPage(
                        plotlyOutput("plot_imc"))
 
             )
+  ),
+  # --- Quinta pestaña --- #
+  tabPanel("Modelo predictivo",
+           sidebarLayout(
+             sidebarPanel(
+               width = 8,
+               h4("Cuestionario de Hábitos"),
+               p("Responde a las siguientes preguntas para que un modelo Random Forest prediga tu categoría:"),
+
+               fluidRow(
+                 column(6,
+                        selectInput("pred_family", "¿Historial familiar con sobrepeso? (family_history_with_overweight)", choices = unique(df$family_history_with_overweight)),
+                        selectInput("pred_FAVC", "¿Consumo frecuente de alimentos ricos en calorías? (FAVC)", choices = unique(df$FAVC)),
+                        selectInput("pred_FCVC", "¿Frecuencia de consumo de vegetales? (FCVC)", choices = levels(df$FCVC)),
+                        selectInput("pred_NCP", "¿Cantidad de comidas principales? (NCP)", choices = levels(df$NCP)),
+                        selectInput("pred_CAEC", "¿Comes entre comidas? (CAEC)", choices = unique(df$CAEC)),
+                        selectInput("pred_SMOKE", "¿Fuma usted? (SMOKE)", choices = unique(df$SMOKE)) 
+                        ),
+                 column(6,
+                        selectInput("pred_CH2O", "¿Cuánta agua bebes diariamente? (CH2O)", choices = levels(df$CH2O)),
+                        selectInput("pred_SCC", "¿Controlas las calorías que consumes? (SCC)", choices = unique(df$SCC)),
+                        selectInput("pred_FAF", "¿Frecuencia de actividad física? (FAF)", choices = levels(df$FAF)),
+                        selectInput("pred_TUE", "¿Tiempo de uso de dispositivos tecnológicos? (TUE)", choices = levels(df$TUE)),
+                        selectInput("pred_CALC", "¿Frecuencia de consumo de alcohol? (CALC)", choices = unique(df$CALC)),
+                        selectInput("pred_MTRANS", "¿Medio de transporte habitual? (MTRANS)", choices = unique(df$MTRANS))
+                        )
+               ),
+               hr(),
+               actionButton("btn_predecir", "Predecir categoría", class = "btn-success", width = "100%")
+             ),
+             mainPanel(
+               width = 4,
+               h2("Resultado del Modelo:"),
+               h3("El modelo predice que perteneces a "),
+               h3(textOutput("texto_resultado_rf"), style = "font-weight: bold;"))
+           )
   )
 )
 
@@ -150,10 +257,6 @@ server = function(input, output) {
   
   # Se usa reactive para que los datos se filtren una única vez.
   # Cada reactive refiere a cada pestaña donde se pueden filtrar los datos
-  datos_filtrados_base = reactive({
-    req(input$NObeyesdad_base)
-    df[df$NObeyesdad %in% input$NObeyesdad_base, ]
-  })
   
   datos_filtrados_exp = reactive({
     req(input$NObeyesdad_exp)
@@ -351,7 +454,7 @@ server = function(input, output) {
   
   # Dataset interactivo en pestaña Base de datos
   output$tabla = renderDT({
-    datos_filtrados_base()
+    df
   })
   
   # Se usa reactive para hacer el cálculo una unica vez
@@ -387,6 +490,40 @@ server = function(input, output) {
       theme_minimal(base_size = 12) 
     
     ggplotly(imc_grafico)
+  })
+  # Modelo de predicción
+  prediccion_rf <- eventReactive(input$btn_predecir, {
+    
+    # Creamos un dataframe de 1 fila con las respuestas ingresadas. 
+    nuevo_dato <- tibble(
+      # Variables que el modelo no usa pero que debemos completar para que funcione
+      Gender = factor(levels(df$Gender)[1], levels = levels(df$Gender)),
+      Age = 25,
+      Height = 1.70,
+      Weight = 70,
+      # Variables que se deben ingresar porque son las que efectivamente usa el modelo
+      family_history_with_overweight = factor(input$pred_family, levels = levels(df$family_history_with_overweight)),
+      FAVC = factor(input$pred_FAVC, levels = levels(df$FAVC)),
+      FCVC = factor(input$pred_FCVC, levels = levels(df$FCVC)),
+      NCP = factor(input$pred_NCP, levels = levels(df$NCP)),
+      CAEC = factor(input$pred_CAEC, levels = levels(df$CAEC)),
+      SMOKE = factor(input$pred_SMOKE, levels = levels(df$SMOKE)),
+      CH2O = factor(input$pred_CH2O, levels = levels(df$CH2O)),
+      SCC = factor(input$pred_SCC, levels = levels(df$SCC)),
+      FAF = factor(input$pred_FAF, levels = levels(df$FAF)),
+      TUE = factor(input$pred_TUE, levels = levels(df$TUE)),
+      CALC = factor(input$pred_CALC, levels = levels(df$CALC)),
+      MTRANS = factor(input$pred_MTRANS, levels = levels(df$MTRANS))
+    )
+    
+    # Hacemos la predicción con el modelo ya entrenado al inicio (wf_rforest2)
+    pred <- predict(wf_rforest2, new_data = nuevo_dato)
+    return(pred$.pred_class[1])
+  })
+  
+  output$texto_resultado_rf <- renderText({
+    req(prediccion_rf())
+    paste(prediccion_rf())
   })
 }
 
